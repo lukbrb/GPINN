b645a97f89cc8dee136dc6edf62b8c168e55858b
lukbrb <lucasbarbr@outlook.com>
2023-03-28 17:17:46 +0200

    add mutligrid plots

--- plots.py
+++ plots.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from models.get_models import dehnen
 from gpinn import potentials
-from error_analysis import relative_error
-
+from error_analysis import relative_error, erreur_difference
+from sklearn.metrics import mean_squared_error
 plt.style.use('seaborn-v0_8-darkgrid')
 # print(plt.style.available)
 
@@ -19,16 +19,25 @@ if len(sys.argv) > 1:
 """
 
 
-def plot_loss(n_epoch, loss_values):
+def plot_loss(n_epoch, loss_values, scale=('linear', 'linear')):
     plt.figure()
     plt.title('Loss as a function of iteration')
     plt.plot(n_epoch, loss_values)
+    plt.xscale(scale[0])
+    plt.yscale(scale[1])
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.show()
 
 
-def plot_prediction(s_domain, y_pred, y_true, gamma):
+def plot_prediction(s_domain, y_pred, y_true, gamma, ax=None):
+    if ax:
+        ax.plot(s_domain, y_pred, '--k', label='Predicted Value')
+        ax.plot(s_domain, y_true, color='red', label='Analytical Value')
+        ax.set_xlabel(r'$s$')
+        ax.set_ylabel(r'$\Phi$')
+        return ax
+
     plt.figure()
     plt.title(f'Dehnen potential for $\gamma={gamma}$')
     plt.plot(s_domain, y_pred, '--k', label='Predicted Value')
@@ -39,7 +48,7 @@ def plot_prediction(s_domain, y_pred, y_true, gamma):
     plt.show()
 
 
-def plot_scatter_s_gamma(s, gamma, diff):
+def plot_scatter_s_gamma(s, gamma, diff, title="Relative Difference"):
     # Création du graphique
     fig, ax = plt.subplots()
 
@@ -52,25 +61,25 @@ def plot_scatter_s_gamma(s, gamma, diff):
 
     # Ajout de la barre de couleur
     cbar = plt.colorbar(scatter)
-    cbar.set_label('Différence relative')
+    cbar.set_label(title)
 
     # Affichage du graphique
     plt.show()
 
 
-def plot_image_s_gamma(s, gamma, diff):
+def plot_image_s_gamma(s, gamma, diff, title="Relative Difference"):
     # Création du graphique
     fig, ax = plt.subplots()
     # Définition des axes
     ax.set_xlabel(r'$s$')
     ax.set_ylabel(r'$\gamma$')
-
     # Tracé de l'image continue colorée par la différence relative entre les prédictions NN et le profil analytique
-    image = ax.imshow(diff, extent=[s.min(), s.max(), gamma.min(), gamma.max()], cmap='coolwarm', origin='lower', aspect='auto')
+    image = ax.imshow(diff, extent=[s.min(), s.max(), gamma.min(), gamma.max()], cmap='coolwarm', origin='lower',
+                      aspect='auto')
 
     # Ajout de la barre de couleur
     cbar = plt.colorbar(image)
-    cbar.set_label('Différence relative')
+    cbar.set_label(title)
 
     # Affichage du graphique
     plt.show()
@@ -80,23 +89,38 @@ if __name__ == "__main__":
     # -------- Plot the loss --------------
     loss = np.load("arrays/loss.npy")
     epochs = np.load("arrays/epochs.npy")
-    plot_loss(epochs, loss)
+    plot_loss(epochs, loss, scale=('linear', 'log'))
 
     # -------- Plot the prediction -----------
-    gamma = 0.5
-    s = np.linspace(1e-2, 10, 1000)
-    y_pred = dehnen(s, gamma=1).detach()
-    y_true = potentials.dehnen(s, gamma=1)
-    plot_prediction(s, y_pred=y_pred, y_true=y_true, gamma=gamma)
+    n_subplots = 3
+    fig, axs = plt.subplots(n_subplots, n_subplots, figsize=(12, 12))
+    gamma = np.linspace(0, 2.99, n_subplots**2)
+    s = np.linspace(1e-1, 10, 1000)
+    for i, ax in enumerate(axs.flat):
+        ax.set_title(f"$\gamma =$ {gamma[i]:.2f}")
+        y_pred = dehnen(s, gamma=gamma[i]).detach()
+        y_true = potentials.dehnen(s, gamma=gamma[i])
+        plot_prediction(s, y_pred=y_pred, y_true=y_true, gamma=gamma[i], ax=ax)
+
+    handles, labels = ax.get_legend_handles_labels()
+    fig.legend(handles, labels, loc='lower center', ncol=3)
+    plt.show()
 
     # -------- Plot the diff depending on gamma and s -----------
     s = np.linspace(1e-2, 10, 1000)
     gammas = np.linspace(0, 2.99, 100)
-    diffs = []
+    abs_diffs = []
+    rel_diffs = []
     for gamma in gammas:
         y_pred = dehnen(s, gamma=gamma).detach().numpy().flatten()
         y_true = potentials.dehnen(s, gamma=gamma)
-        diff = relative_error(y_pred=y_pred, y_true=y_true)
-        diffs.append(diff)
-    diffs = np.array(diffs)
-    plot_image_s_gamma(s, gammas, diffs)
+        abs_diff = erreur_difference(y_pred=y_pred, y_true=y_true)
+        rel_diff = relative_error(y_pred=y_pred, y_true=y_true)
+        print(mean_squared_error(y_true=y_true, y_pred=y_pred))
+        abs_diffs.append(abs_diff)
+        rel_diffs.append(rel_diff)
+
+    abs_diffs = np.array(abs_diffs)
+    rel_diffs = np.array(rel_diffs)
+    plot_image_s_gamma(s, gammas, abs_diffs, title='Absolute difference')
+    plot_image_s_gamma(s, gammas, rel_diffs, title='Relative difference')
