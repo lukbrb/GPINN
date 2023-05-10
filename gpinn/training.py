ad273d6f646a6a4a1c7e2a662113c22edb0370f9
lukbrb <lucasbarbr@outlook.com>
2023-05-10 19:39:06 +0200

    add validation loss and accuracy in training

--- gpinn/training.py
+++ gpinn/training.py
@@ -7,7 +7,7 @@ class TrainingPhase:
     def __init__(self, neural_net, *, training_points, testing_points, equation, n_epochs, _loss_function):
         self.neural_net = neural_net
         self.X_train_Nu, self.Y_train_Nu, self.X_train_Nf = training_points
-        self.X_test, self.Y_test = testing_points
+        self.X_val, self.Y_val = testing_points
         self.pde = equation
         self.n_epochs = n_epochs
         self.loss_function = _loss_function
@@ -27,29 +27,42 @@ class TrainingPhase:
         return loss_bc + loss_pde
 
     # Optimizer              X_train_Nu,Y_train_Nu,X_train_Nf
-    def closure(self, optimizer):
-        optimizer.zero_grad()
-        loss = self.loss(self.X_train_Nu, self.Y_train_Nu, self.X_train_Nf)
-        loss.backward()
-        self.iter += 1
-        if self.iter % 100 == 0:
-            loss2 = self.loss_boundary(self.X_test, self.Y_test)
-            print("Training Error:", loss.detach().cpu().numpy(), "---Testing Error:", loss2.detach().numpy())
-        return loss
+    # def closure(self, optimizer):
+    #     optimizer.zero_grad()
+    #     loss = self.loss(self.X_train_Nu, self.Y_train_Nu, self.X_train_Nf)
+    #     loss.backward()
+    #     self.iter += 1
+    #     if self.iter % 100 == 0:
+    #         loss2 = self.loss_boundary(self.X_test, self.Y_test)
+    #         print("Training Error:", loss.detach().cpu().numpy(), "---Testing Error:", loss2.detach().numpy())
+    #     return loss
 
     def train_model(self, optimizer, learning_rate):
         opt = optimizer(self.neural_net.parameters(), lr=learning_rate)
-
-        losses = []
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(opt, mode='min', factor=0.5, patience=1000,
+                                                               threshold=1e-3, verbose=False)
+        train_losses = []
+        valid_losses = []
         epochs = []
+        accuracy = []
         for epoch in trange(self.n_epochs):
             opt.zero_grad()
             loss_value = self.loss(self.X_train_Nu, self.Y_train_Nu, self.X_train_Nf)
             loss_value.backward()
             opt.step()
-            losses.append(loss_value.item())
+            scheduler.step(loss_value)
+
+            # Compute stats on learning
+            diff = self.Y_val - self.neural_net(self.X_val).detach()
+            precision = np.abs(diff / self.Y_val)
+            loss_value_valid = self.loss_function(diff)
+
+            accuracy.append(precision.mean().detach())
+            valid_losses.append(loss_value_valid.item())
+            train_losses.append(loss_value.item())
             epochs.append(epoch)
-        return self.neural_net, np.array(epochs), np.array(losses)
+
+        return self.neural_net, np.array(epochs), np.array(train_losses), np.array(valid_losses), np.array(accuracy)
 
     def save_model(self, filename):
         torch.save(self.neural_net, filename)
